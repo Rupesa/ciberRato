bb84c4d788dec451d0759f048851bc6e7077e4a0
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-01-16 13:15:51 +0000

    scripts done

---
+++ challenge4/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# How to build your project
+
+# Possibility for cpp agents
+# make mainC4 
+
+# Possibility for java agents
+# javac mainC4.java
+
+# Possibility for python agents
+# nothing
+
+
---
+++ challenge4/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+challenge="4"
+host="localhost"
+robname="theAgent"
+pos="0"
+outfile="solution"
+
+while getopts "c:h:r:p:f:" op
+do
+    case $op in
+        "c")
+            challenge=$OPTARG
+            ;;
+        "h")
+            host=$OPTARG
+            ;;
+        "r")
+            robname=$OPTARG
+            ;;
+        "p")
+            pos=$OPTARG
+            ;;
+        "f")
+            outfile=$OPTARG
+            ;;
+        default)
+            echo "ERROR in parameters"
+            ;;
+    esac
+done
+
+shift $(($OPTIND-1))
+
+case $challenge in
+    4)
+        # how to call agent for challenge 4
+        python3 mainClientC4.py -h "$host" -p "$pos" -r "$robname"
+        mv output.map $outfile.map
+        mv output.path $outfile.path
+        ;;
+        
+esac
+
